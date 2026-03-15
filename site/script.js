8802a25d4969027b44d473a77c291e6fe13d85af
galgmh <2335345445@qq.com>
2026-03-15 15:49:31 +0800

    Refactor leaderboard rendering and sorting functions

--- site/script.js
+++ site/script.js
@@ -1,7 +1,7 @@
 let data=[]
 
 fetch("data/leaderboard.json")
-.then(r=>r.json())
+.then(res=>res.json())
 .then(json=>{
 
 data=json
@@ -10,30 +10,29 @@ data.forEach(d=>{
 d.score=(d.AUC+d.F1)/2
 })
 
-render(data)
+renderTable(data)
 
 })
 
-function render(rows){
+function renderTable(rows){
 
 let tbody=document.querySelector("#leaderboard tbody")
 tbody.innerHTML=""
 
-rows
-.sort((a,b)=>b.score-a.score)
-.forEach((d,i)=>{
+rows.forEach((d,i)=>{
 
 let tr=document.createElement("tr")
 
 tr.innerHTML=`
+<td>${i+1}</td>
 <td>${d.detector}</td>
 <td>${d.organization}</td>
 <td>${d.AUC.toFixed(3)}</td>
 <td>${d.F1.toFixed(3)}</td>
 <td>${d.score.toFixed(3)}</td>
 <td>${d.dataset}</td>
-<td><a href="${d.paper}">paper</a></td>
-<td><a href="${d.code}">code</a></td>
+<td><a href="${d.paper}" target="_blank">paper</a></td>
+<td><a href="${d.code}" target="_blank">code</a></td>
 `
 
 tbody.appendChild(tr)
@@ -42,10 +41,10 @@ tbody.appendChild(tr)
 
 }
 
-function sortTable(key){
+function sortTable(metric){
 
-data.sort((a,b)=>b[key]-a[key])
+data.sort((a,b)=>b[metric]-a[metric])
 
-render(data)
+renderTable(data)
 
 }
